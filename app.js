5b0a0f329d3a3d97e9c2c02945a31395516ebbc3
stephaniealbarran <stefanyalbarran@hotmail.com>
2022-06-17 18:04:16 +0000

    ejercicio solucionado

---
+++ app.js
@@ -0,0 +1,19 @@
+     let createDomain = () => {
+        let pronoun = ["the", "our"];
+        let adj = ["great", "big"];
+        let noun = ["nutria", "alpaca"];
+        let dom = [".com", ".es", ".org"];
+      
+
+        for (let i = 0; i < pronoun.length; i++) {
+          for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+              for (let h = 0; h < dom.length; h++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + dom[h]);
+              }
+            }
+          }
+        }
+        return createDomain;
+      };
+    console.log(createDomain());
